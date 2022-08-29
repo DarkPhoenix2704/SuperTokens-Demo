34084e98350f2617d7dca29c4c336f632130339a
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-08-30 00:37:06 +0530

    feat: basic supertokens

---
+++ api-server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+let supertokens = require("supertokens-node");
+let Session = require("supertokens-node/recipe/session");
+let { verifySession } = require("supertokens-node/recipe/session/framework/express");
+let { middleware, errorHandler } = require("supertokens-node/framework/express");
+let EmailPassword = require("supertokens-node/recipe/emailpassword");
+let Dashboard = require("supertokens-node/recipe/dashboard");
+
+const apiPort = process.env.REACT_APP_API_PORT || 3001;
+const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+const websiteDomain = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+
+supertokens.init({
+    framework: "express",
+    supertokens: {
+        connectionURI: "https://d54af56127a511ed92caad2c4ea25512-ap-southeast-1.aws.supertokens.io:3572",
+        apiKey: "U9fogcuqAj4Ep9Xd49th8qrm8qltoV",
+    },
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain,
+        websiteDomain,
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init(),
+        Dashboard.init({
+            apiKey: "supertokens_is_awesome",
+        }),
+    ],
+});
+
+const app = express();
+
+app.use(
+    cors({
+        origin: websiteDomain,
+        allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(morgan("dev"));
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+app.use(middleware());
+
+// An example API that requires session verification
+app.get("/sessioninfo", verifySession(), async (req, res) => {
+    let session = req.session;
+    res.send({
+        sessionHandle: session.getHandle(),
+        userId: session.getUserId(),
+        accessTokenPayload: session.getAccessTokenPayload(),
+    });
+});
+
+app.use(errorHandler());
+
+app.use((err, req, res, next) => {
+    res.status(500).send("Internal error: " + err.message);
+});
+
+app.listen(apiPort, () => console.log(`API Server listening on port ${apiPort}`));
